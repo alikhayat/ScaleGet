231346fed1d260bfd2fa5ab69a9c8616fdba5200
alikhayat <ali.khayat.2@gmail.com>
2020-08-28 19:04:50 +0300

    V0.5
    Excel saving

--- ScaleGet/ScaleGet/AveryRetrieve.vb
+++ ScaleGet/ScaleGet/AveryRetrieve.vb
@@ -1,4 +1,9 @@
-﻿Option Strict On
+﻿Option Strict Off
+
+Imports System.Net.Mime.MediaTypeNames
+Imports Microsoft.Office.Interop
+Imports System.Reflection
+
 Public Class AveryRetrieve
     Dim GetP As Byte() = {&H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0, &H0,
                           &H0, &H0, &H0, &H0, &H0, &H0, &H1D, &HB0, &H0, &H3C, &H64, &HFE, &H20, &H49, &H0, &H0, &HD, &H51, &H0, &H0, &H1, &H1, &H4E,
@@ -30,13 +35,64 @@ Public Class AveryRetrieve
         Return RequestSize
     End Function
     Public Sub RetrieveData(ByVal Address As String, ByVal Port As String, ByVal ScaleNo As Int16)
+        Dim Data As New Dictionary(Of UInt16, UInt32)
+        Dim Plu As UInt16 = 0
+        Dim Price As UInt32 = 0
+        Dim HexPlu() As Byte = New Byte() {0, 0}
+        Dim HexPrice() As Byte = New Byte() {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
         Dim Comm As New communicator(Address, Port)
         Dim DataList As New List(Of Byte())
-        DataList = Comm.InializeComm(Main.ScaleType.Avery)
-        'If IsNothing(DataList) = False Then
-        '    For i As Integer = 0 To DataList.Count - 1
+        Try
+            DataList = Comm.InializeComm(Main.ScaleType.Avery)
+            If IsNothing(DataList) = False Then
+                For i As Integer = 0 To DataList.Count - 1
+                    'Fill Byte Array's
+                    HexPlu(0) = DataList.Item(i)(99)
+                    HexPlu(1) = DataList.Item(i)(98)
 
-        '    Next
-        'End If
+                    HexPrice(0) = DataList.Item(i)(108)
+                    HexPrice(1) = DataList.Item(i)(107)
+                    HexPrice(2) = DataList.Item(i)(106)
+
+                    'Convert Byte Arrays to their decimal Value
+                    Plu = Convert.ToUInt16(BitConverter.ToUInt16(HexPlu, 0))
+                    Price = Convert.ToUInt32(BitConverter.ToUInt32(HexPrice, 0))
+
+                    'Fill Dictionary
+                    Data.Add(Plu, Price)
+                Next
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+
+        'Produce excel sheet
+
+        Dim excelapp As New Excel.Application
+        Dim WB As Excel.Workbook
+        Dim WS As Excel.Worksheet
+        Dim column As Integer = 1
+
+        WB = excelapp.Workbooks.Add(Missing.Value)
+        'WB = WB.Sheets()
+        WS = WB.ActiveSheet
+
+        excelapp.Visible = True
+        'excelapp.UserControl = True
+        Dim row As Integer = 1
+        For Each key As UInt16 In Data.Keys
+            WS.Cells(row, column).Value = key
+            row += 1
+        Next
+        column = 2
+        row = 1
+        For Each value As UInt32 In Data.Values
+            WS.Cells(row, column).Value = value
+            row += 1
+        Next
+        WS.SaveAs("Test")
+    End Sub
+    Private Sub SurroundingSub()
+
     End Sub
 End Class
--- ScaleGet/ScaleGet/Form1.vb
+++ ScaleGet/ScaleGet/Form1.vb
@@ -5,11 +5,12 @@
         ToledoBPlus
     End Enum
     Private Sub Main_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        Dim b() As Byte = New Byte() {&H1, &H2, 3, 4}
+        ' MsgBox(BitconverterExt.ToDecimal(b).ToString)
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Dim Avery As New AveryRetrieve
-        Avery.RetrieveData("10.3.5.7", "3001", 7)
+        Avery.RetrieveData("10.3.5.3", "3001", 3)
     End Sub
 End Class
